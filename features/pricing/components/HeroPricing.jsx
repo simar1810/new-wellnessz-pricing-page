dbd8d06d10c62b415371afe8a5e527c0f50f511a
Mannat Jaiswal <mannatjaiswal03@gmail.com>
2026-06-01 14:35:20 +0530

    fix: added space above trusted partners in mobile view

--- features/pricing/components/HeroPricing.jsx
+++ features/pricing/components/HeroPricing.jsx
@@ -22,7 +22,7 @@ function pauseAndMuteVideos(root) {
   });
 }
 
-const HeroPricing = function() {
+const HeroPricing = function () {
   const hasMounted = useHasMounted();
   const videoSectionRef = useRef(null);
 
@@ -46,7 +46,7 @@ const HeroPricing = function() {
   }, []);
 
   return (
-    <section className="relative w-full p-4">
+    <section className="relative w-full p-4 short:mb-10">
       <div className="relative mx-auto max-w-[1400px] rounded-[20px] bg-linear-to-br pb-0 pt-16 text-center text-white md:rounded-[40px] md:pt-24 short:pb-16">
         <Image
           fill
